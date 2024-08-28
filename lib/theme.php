c8185ee70efe3c75009c4b4b52588cef57051b20
kurst-mnfurs <50988033+kurst-mnfurs@users.noreply.github.com>
2024-08-28 00:54:01 -0500

    Improve upload functionality.

--- lib/theme.php
+++ lib/theme.php
@@ -1,24 +1,26 @@
 <?php
 /* Used for theming */
+function OnlineSched_terms_list($term, &$masterList = null): string
+{
+	$fields = ($masterList === null) ? 'names' : 'all';
+	$tags_arr = wp_get_post_terms(get_the_ID(), $term, ['fields' => $fields]);
 
-function OnlineSched_terms_list($term) {
-        $tags_arr = wp_get_post_terms(get_the_ID(), $term);
-        $tags = '';
-        if ($tags_arr) {
-                $first_entry = true;
-                for ($i = 0; $i < count($tags_arr); $i++) {
-                        if ($first_entry == false) {
-                                $tags .= ', ';
-                        }
-                        $tags .= $tags_arr[$i]->name;
-                        $first_entry = false;
-                }
-        } else {
-                $tags .= 'None';
-        }
+	if (empty($tags_arr)) {
+		return 'None';
+	}
 
-        return $tags;
+	$tag_names = [];
+	foreach ($tags_arr as $tag) {
+		$tag_names[] = $tag->name;
+
+		if ($masterList !== null) {
+			$masterList[$tag->name] = $tag->slug;
+		}
+	}
+
+	return implode(', ', $tag_names);
 }
+
 
 // return as array
 function OnlineSched_terms_slug_array($term) {
