2660efa9b02dae5c0474ead34fdb7e5e7e6785b7
kurst-mnfurs <50988033+kurst-mnfurs@users.noreply.github.com>
2025-08-29 22:56:30 -0500

    Tweeks of help

--- templates/page-schedule.php
+++ templates/page-schedule.php
@@ -835,15 +835,14 @@ $badge_types_fg_colors = get_option('onlinesched_badge_types_fg_colors', array()
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" id="info-modal-close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h3 class="modal-title">How Favorites, Login, and Calendar Work</h3>
+                    <h3 class="modal-title">How Favorites, Login, Calendar, and Sharing Work</h3>
                 </div>
                 <div class="modal-body">
                     <div style="font-size:1.1em;">
-                        <p><strong>Favorites:</strong> You can mark events as favorites to keep track of your schedule. If you’re not logged in, your favorites are saved only on this device. If you log in, your favorites are saved to your account and sync across devices.</p>
-                        <p><strong>Login:</strong> Logging in lets you save your schedule and favorites to your account, so you can access them from any device.<br />
-                            The login information is only used to identify you. We do not use that information any more than that.</p>
-                        <p><strong>Calendar:</strong> You can add events or your entire schedule to your calendar (Google, Apple, Outlook). You can select an individual calendar feed item by hitting the Google or Apple Icon (Which works for Apple). Or the filtered list is avaiable on the bottom of the page. If you don't select a filter list all the calendar items will be added from the links on the bottom of the page.  Calendar feeds update periodically, but may not always reflect real-time changes. For the latest info, check this website.</p>
-
+                        <p><strong><i class="far fa-star" aria-hidden="true"></i> Favorites:</strong> Mark events as favorites to keep track of your schedule. If you’re not logged in, your favorites are saved only on this device. If you log in, your favorites are saved to your account and sync across devices.</p>
+                        <p><strong><i class="fas fa-sign-in-alt" aria-hidden="true"></i> Login:</strong> Logging in lets you save your schedule and favorites to your account, so you can access them from any device. Your login info is only used to identify you, nothing more! And won't be kept past the convention.</p>
+                        <p><strong><i class="far fa-calendar-alt" aria-hidden="true"></i> Calendar:</strong> Add events or your whole schedule to your calendar (Google, Apple, Outlook). You can add individual events by tapping the calendar icons, or add everything at once from the bottom of the page. Calendar feeds update periodically, but may not always reflect real-time changes. For the latest info, check this website.</p>
+                        <p><strong><i class="fas fa-copy" aria-hidden="true"></i> Share:</strong> Want to share an event with a friend? Tap the copy icon to grab the event link and paste it anywhere like social media, email, chat, on side of your car, Rico's hand, or stiched on your fursuit. No tech wizardry required!</p>
                     </div>
                 </div>
             </div>
