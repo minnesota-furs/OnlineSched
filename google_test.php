8c5bab909e75972ca388984e3111219dd7282c2a
kurst-mnfurs <50988033+kurst-mnfurs@users.noreply.github.com>
2025-08-31 17:23:52 -0500

    testing google encoding

---
+++ google_test.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<html>
+<head>
+
+</head><body>
+<a href="https://calendar.google.com/calendar/r?cid=webcal%3A%2F%2Fwww.furrymigration.org%2Fwp-content%2Fplugins%2FOnlineSched%2Ficalby.php%3Froom%3Dall">Webdav</a>
+<br /><br/>
+<a href="https://calendar.google.com/calendar/r?cid=https%3A%2F%2Fwww.furrymigration.org%2Fwp-content%2Fplugins%2FOnlineSched%2Ficalby.php%3Froom%3Dall">https</a>
+<br /><br />
+
+<a href="https://calendar.google.com/calendar/r?cid=http%3A%2F%2Fwww.furrymigration.org%2Fwp-content%2Fplugins%2FOnlineSched%2Ficalby.php%3Froom%3Dall">http</a>
+
+</body>
+</html>
